1f0f70670abb0ed9c9e1e17e0cc7049afbaa3f61
Your Name <you@example.com>
2022-02-25 23:27:33 +0600

    update something

--- calculator.py
+++ calculator.py
@@ -3,3 +3,81 @@ print(6/3)
 print(6//3)
 print(5/3)
 print(5//3)
+#new lesseon
+print("Today's lessone")
+for i in range(2,30,2):
+    print(i)
+
+print("loop")
+for i in range(1,10,4):
+    print(i)
+
+print("range")
+for i in range(1,5,2):
+    print(i)
+
+""" print("infinit loop")
+while True:
+    print("please enter name:")
+    name=input()
+    if name == "you name":
+        break
+print("thank you")
+
+print("again")
+
+while True:
+    print("Who are you?")
+    name = input()
+    if name != 'akash':
+        continue
+    print("hello"+ name +"what is your pass code?")
+    passcode = input()
+    if passcode == "1234":
+        break
+
+print("enter akash")
+"""
+"""
+while True:
+    print("who are you?")
+    name = input()
+    if name != 'kamal':
+        continue
+    print("Hi "+ name + " how are you?")
+    password = input()
+    if password == "fine":
+        break
+
+print("thank you " + name)
+"""
+"""
+print("loop")
+sum = 0
+for i in range(1,10,1):
+    print(sum)
+    sum = sum + i * i
+
+print(sum)
+
+
+
+def f():
+    print("hello")
+
+f()
+
+def sum(a,b):
+    return a+b
+
+a = sum(1,2)
+print(a)
+
+def print_my_name(name):
+    print('my name is  '+ name)
+
+my_name =input()
+print_my_name(my_name)
+"""
+var =print("ok")
+print(type(var))
