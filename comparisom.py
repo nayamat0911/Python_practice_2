31692120f396599ca27c099caa0b87bf5d10a9e3
Your Name <you@example.com>
2022-02-26 17:35:55 +0600

    data type

---
+++ comparisom.py
@@ -0,0 +1,55 @@
+print("logical oparator")
+print("AND, OR, NOT")
+a=20
+b=10
+print(a==20 and b==10) # all crrect is currect
+print(a==20 and b==4)
+print(a==20 or b==10) # one currect is currect
+print(a==20 or b==4)
+print(not a==20)
+print(not b==30)
+
+print("membership oparation")
+print("in , not in")
+a = "nayamat"
+print(a)
+print("naya" in a)
+print("nayam" not in a)
+print("code" in a)
+print("codeing" not in a)
+
+print("identity operator")
+print("identity operatr = is, is not")
+n=5
+c=7
+print("555" is n)
+print(n is 5)
+print('is')
+print(5 is not c)
+
+print("Bitwise id")
+a =30
+b=14
+print(a & b)
+x=20
+y=40
+z=50
+z +=x
+z -=y
+print(z)
+print(z)
+
+print("data type")
+print("nine", 'boolean', "number", 'string', 'list', 'tuple', 'set', 'dictionary', sep=',')
+print("None data type : ")
+a =10
+print(a)
+a = None
+print(a)
+print(type(a))
+if a == None:
+    print("yes")
+else:
+    print("none")
+
+
